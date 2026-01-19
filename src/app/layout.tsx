6a886ff9a9e8719eee68713f4ac581961238fc86
slowwalker77 <90623845+slowwalker77@users.noreply.github.com>
2026-01-20 08:00:55 +0900

    feat: add GA4 google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Container from '@/components/ui/container';
 
+// ✅ GA4 삽입을 위한 Script
+import Script from 'next/script';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -24,6 +27,20 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
+
+        {/* ✅ Google tag (gtag.js) - GA4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-M2XMR4D8DD"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M2XMR4D8DD');
+          `}
+        </Script>
       </body>
     </html>
   );
